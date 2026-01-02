255e58bc919a6be1523a1e332b3f7227b0df1752
tgel0 <gelo.tomi@gmail.com>
2026-01-02 17:36:52 +0100

    Configure Firebase Hosting and SvelteKit static adapter for deployment

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
@@ -11,6 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 // const analytics = getAnalytics(app);
 
-export { app };
+export { app, db };
